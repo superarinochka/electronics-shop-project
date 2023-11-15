832bddc876924c6763d1facf8faf618de92c5bf6
Arina Astanina <timyr.kiyan@gmail.com>
2023-11-16 00:02:39 +0300

    Add HW2

--- src/item.py
+++ src/item.py
@@ -1,38 +1,56 @@
+import csv
+import os
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
-
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-
         self.all.append(self)
 
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <=10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number))
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path):
+        cls.all.clear()
+        file_path = os.path.join(os.path.dirname(__file__), "..", file_path)
+        with open(file_path, "r", encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item(row['name'], float(row['price']), int(row['quantity']))
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
-
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * self.pay_rate
-
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,38 @@
 from src.item import Item
+import pytest
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
-def test_item2():
-    item1 = Item("Смартфон", 10000, 20)
-    assert item1.price == 10000
+@pytest.fixture
+def item():
+    return Item("Смартфон", 1000.0, 20)
+
+def test_init(item):
+    assert item.price == 1000.0
+    assert item.name == "Смартфон"
+    assert item.quantity == 20
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 20000
+    assert type(item.calculate_total_price()) is float
+
+
+def test_name(item):
+    item.name = "Смартфонсмартфон"
+    assert item.name == "Смартфонсм"
+    assert len(item.name) == 10
+
+
+def test_apply_discount(item):
+    assert item.price == 1000.0
+    item.pay_rate = 0.15
+    item.apply_discount()
+    assert item.price == 150.0
+
+def test_string_to_number(item):
+    assert item.string_to_number("5.0") == 5
+    assert item.string_to_number("6.5") == 6
+
+def test_instantiate_from_csv(item):
+    item.instantiate_from_csv("src/items.csv")
+    assert len(Item.all) == 5
